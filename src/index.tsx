d33e2eb6f5d934bfeedf772cdcf3e92a0872c311
TimarTimar <tamasbelinszky@gmail.com>
2021-04-24 01:28:50 +0200

    cors3

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,6 @@ import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
 	uri: process.env.REACT_APP_SERVER_URL,
-	credentials: "same-origin",
 });
 
 const authLink = setContext(() => {
