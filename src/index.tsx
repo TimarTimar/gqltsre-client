816951d616615c0be36cf22b527b5fbdb3c1070a
TimarTimar <tamasbelinszky@gmail.com>
2021-04-24 02:12:44 +0200

    cors5

--- src/index.tsx
+++ src/index.tsx
@@ -14,12 +14,24 @@ const httpLink = createHttpLink({
 	uri: process.env.REACT_APP_SERVER_URL,
 	credentials: "include",
 });
-
+/*
 const authLink = setContext(() => {
 	const token = localStorage.getItem("jwtToken");
 	return {
 		headers: {
 			Authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});*/
+
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem("token");
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
 		},
 	};
 });
